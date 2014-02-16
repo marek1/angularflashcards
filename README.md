629d81f7073efe840cf9b3b923b1def5f9ee24b2
marek1 <cyclingfellas@gmail.com>
2014-02-16 14:31:06 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Angular Flashcard App
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('angularflashcardsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
